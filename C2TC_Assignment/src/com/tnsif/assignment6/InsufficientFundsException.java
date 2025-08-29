8ab1331015f0f56240e322ac3c4f708fc8787b17
Arjunatk25 <144626188+Arjunatk25@users.noreply.github.com>
2025-08-29 09:10:18 +0530

    COMMIT

---
+++ C2TC_Assignment/src/com/tnsif/assignment6/InsufficientFundsException.java
@@ -0,0 +1,9 @@
+package com.tnsif.assignment6;
+
+public class InsufficientFundsException extends Exception {
+	public InsufficientFundsException(String message) {
+		super(message);
+		
+	}
+
+}
---
+++ C2TC_Assignment/src/com/tnsif/assignment6/InvalidAmountException.java
@@ -0,0 +1,8 @@
+package com.tnsif.assignment6;
+
+public class InvalidAmountException extends Exception{
+	public InvalidAmountException(String message) {
+		super(message);
+	}
+
+}
